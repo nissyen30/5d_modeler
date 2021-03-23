158c34233b7254b11b3438c4b2e521f3965ddf70
Josh Mosgrove <nissyen27@yahoo.com>
2021-03-23 16:47:22 -0400

    First commit

---
+++ 5d_modeler.py
@@ -0,0 +1,9 @@
+# Ask user to select from equation options
+# Give 3 options, linear (ax + b), quadratic (ax^2 + bx + c), & a non-linear equation (ax^5 + bx^4 + cx^3 + dx^2 + ex + f)
+
+
+# Ask user to input initial value.
+
+# Calculate 5 successive iterations for the value.
+
+# Output the successive iterations.
