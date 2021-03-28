c2b1d9caf7573abcaf17cfb23563d9cebb5457e9
Josh Mosgrrove <nissyen30@yahoo.com>
2021-03-28 15:57:17 -0400

    Updated README

--- 5d_modeler.py
+++ 5d_modeler.py
@@ -6,9 +6,7 @@ def sphere(radius):
     sphere_volume = 12.56/3 * r ** 3
     sphere_sa = 12.56 * r ** 2
     sphere_volume = round(sphere_volume, 2)
-    results = {'Radius':r, 'Volume':sphere_volume, 'Surface Area':sphere_sa}
-    print(results)
-    return sphere_volume, sphere_sa
+    sphere_results.update({'Radius':r, 'Volume':sphere_volume, 'Surface Area':sphere_sa})
 
 def r_prism(x_length, y_width, z_height):
     x = float(x_length)
@@ -19,8 +17,8 @@ def r_prism(x_length, y_width, z_height):
     results = {'Length':r, 'Volume':sphere_volume, 'Surface Area':sphere_sa}
     return prism_volume, prism_sa
 
-def log_results():
-    test_value = 'Got it'
+def prt_log():
+    # print log after each loop
     with open("5d_results.txt", "a") as file:
         file.write(test_value+'\n')
 
@@ -35,33 +33,44 @@ print ("These equations will vary over time based off the options chosen for the
 print ("You may continue as long as you wish or enter 'q' to quit the application.\n")
 
 # Ask user to input initial value.
-equation = input("Would you prefer to use an equation for a sphere(1), an equation for a rectangular prism(2), or would you like to quit(q)?\nPlease enter a 1, 2, or q only.  ").lower()
+while True:
+    equation = input("Would you prefer to use an equation for a sphere(1), an equation for a rectangular prism(2), or would you like to quit(q)?\nPlease enter a 1, 2, or q only.  ").lower()
 
-if equation != "q":
-    time = input("And what value would you like to use for delta t (change in time) in these equations?  ")
+    if equation == "q":
+        print("Thanks for playing!")
+        break
 
-    if equation == "1":
-        radius = input("Please enter the initial radius you would like to use.  ")
-        radius = float(radius)
-        d_radius = input("Please enter the amount you would like the radius to change each time.  ")
-        d_radius = float(d_radius)
-        sphere(radius)
-        # print (sphere_volume)
-        # print (sphere_ra)
+    while equation == "1" or equation =="2":
+
+        # elif equation != "q" and equation != "1" and equation != "2"
+
+        if equation == "1" or equation == "2":
+            time = input("And what value would you like to use for delta t (change in time) in these equations?  ")
+
+        if equation == "1":
+            radius = input("Please enter the initial radius you would like to use.  ")
+            radius = float(radius)
+            d_radius = input("Please enter the amount you would like the radius to change each time.  ")
+            d_radius = float(d_radius)
+            sphere_results = dict()
+            sphere(radius)
+            # print (sphere_results)
+            
+
+        elif equation == "2":
+            print ("For a rectangular prism, please choose the intial value of each side (x, y, z) and the change of each of these values.")
+            x_length = input("What is the initial length (x) for the prism?  ")
+            dx = input("What is the change in x (delta x) after each iteration?  ")
+            y_width = input("What is the initial width (y) for the prism?  ")
+            dy = input("What is the change in y (delta y) after each iteration?  ")
+            z_height = input("What is the initial height (z) for the prism?  ")
+            dz = input("What is the change in z (delta z) after each iteration?  ")
+            r_prism(x_length, y_width, z_height)
         
 
-    elif equation == "2":
-        print ("For a rectangular prism, please choose the intial value of each side (x, y, z) and the change of each of these values.")
-        x_length = input("What is the initial length (x) for the prism?  ")
-        dx = input("What is the change in x (delta x) after each iteration?  ")
-        y_width = input("What is the initial width (y) for the prism?  ")
-        dy = input("What is the change in y (delta y) after each iteration?  ")
-        z_height = input("What is the initial height (z) for the prism?  ")
-        dz = input("What is the change in z (delta z) after each iteration?  ")
-        r_prism(x_length, y_width, z_height)
-        
+        # Calculate 5 successive iterations for the value.
+        # while i != 5:
 
-# Calculate 5 successive iterations for the value.
-# Will call functions 5 times and output the equations as a list.
+        # Will call functions 5 times and output the equations as a list.
 
-# Output the successive iterations.
+        # Output the successive iterations.
--- README.txt
+++ README.txt
@@ -1,7 +1,12 @@
 This is my Python class project. It is a basic 5d Modeler.
 
-The idea is to offer the user the choice of 3 different customizable equations through a Python console app.
-
-The user will choose the equation, the value of the coefficients, the starting value, the change in time, and the change from the starting value over five iterations.
+The idea is to offer the user a choice to model either a sphere or rectangulal prism then the user will choose the starting values, the change in time, and the change from the starting value over five iterations.
 
 The app will process the output of the starting value plus four more iterations of the starting value plus the change (delta) to create five separate sets of space-time equations.
+
+The requirements that will be met are:
+	1. Implement a “master loop” console application where the user can repeatedly enter commands/perform actions, including choosing to exit the program
+	2. Create a dictionary or list, populate it with several values, retrieve at least one value, and use it in your program
+	3. Implement a log that records errors, invalid inputs, or other important events and writes them to a text file
+	4. Read data from an external file, such as text, JSON, CSV, etc and use that data in your application
+	5. Create and call at least 3 functions or methods, at least one of which must return a value that is used somewhere else in your code. To clarify, at least one function should be called in your code, that function should calculate, retrieve, or otherwise set the value of a variable or data structure, return a value to where it was called, and use that value somewhere else in your code. For example, you could create a function that reads how many items there are in a text file, returns that value, and later uses that value to execute a loop a certain number of times.
