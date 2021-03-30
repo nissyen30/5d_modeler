7d5123d4b8c2b6b381f5eab0cf1e999385194169
Josh Mosgrrove <nissyen30@yahoo.com>
2021-03-29 22:10:44 -0400

    Will polish tomorrow

--- 5d_modeler.py
+++ 5d_modeler.py
@@ -1,19 +1,21 @@
 import math
 import json
 
-# Create functions for 2 equations and print option
+
 def sphere(radius, d_radius):
     j = 1
     r = float(radius)
     dr = float(d_radius)
 
     while j < 4:
-        sphere_volume = 12.56/3 * r ** 3
-        sphere_sa = 12.56 * r ** 2
-        sphere_volume = round(sphere_volume, 2)
-        sphere_dict.update({'Radius':r, 'Volume':sphere_volume, 'Surface Area':sphere_sa})
-        sphere_list.append(sphere_dict)
-        prt_log(sphere_dict)
+        r1 = ("Radius " + str(j))
+        v1 = ("Volume " + str(j))
+        s1 = ("Surface Area " + str(j))
+        sphere_volume = round((12.56/3 * r ** 3), 2)
+        sphere_sa = round((12.56 * r ** 2), 2)
+        sphere_dict[r1] = r
+        sphere_dict[v1] = sphere_volume
+        sphere_dict[s1] = sphere_sa
         r += dr
         j += 1
 
@@ -27,26 +29,29 @@ def r_prism(x_length, dx, y_width, dy, z_height, dz):
     dz = float(dz)
 
     while k < 4:
-        prism_volume = x * y * z
-        prism_sa = (2 * x * y) + (2 * x * z) + (2 * y * z)
-        prism_dict.update({'Length':x, 'Width':y, 'Height':z, 'Volume':prism_volume, 'Surface Area':prism_sa})
-        prism_list.append(prism_dict)
-        prt_log(prism_dict)
-        print (prism_list)
+        x2 = ("Length " + str(k))
+        y2 = ("Width " + str(k))
+        z2 = ("Height " + str(k))
+        v2 = ("Volume " + str(k))
+        s2 = ("Surface Area " + str(k))
+        prism_volume = round((x * y * z), 2)
+        prism_sa = round(((2 * x * y) + (2 * x * z) + (2 * y * z)), 3)
+        prism_dict[x2] = x
+        prism_dict[y2] = y
+        prism_dict[z2] = z
+        prism_dict[v2] = prism_volume
+        prism_dict[s2] = prism_sa
         x += dx
         y += dy
         z += dz
         k += 1
 
-
 def prt_log(my_list):
-    # print log after each loop
+    # print log to 5d_modeler.txt
     with open("5d_results.txt", "a") as file:
         file.write(json.dumps(my_list))
 
 # Ask user to select from equation options
-# Easier for people to visualize - use spherical equation or a rectangular prism
-# Should I ask user to stick to integers?
 print ("This application will output 5 different sets of changes in the size of a 3 dimensional object over time.")
 print ("You will be able to choose between an equation for a sphere of varying radius or a rectangular prism of varying dimensions.")
 print ("These equations will vary over time based off the options chosen for the change in time and dimension(s).\n")
@@ -56,42 +61,47 @@ print ("You may continue as long as you wish or enter 'q' to quit the applicatio
 
 # Ask user to input initial value.
 while True:
-    equation = input("Would you prefer to use an equation for a sphere(1), an equation for a rectangular prism(2), or would you like to quit(q)?\nPlease enter a 1, 2, or q only.  ").lower()
+    equation = input("Would you prefer to model a sphere(1), a rectangular prism(2), or would you like to quit(q)?\nPlease enter a 1, 2, or q only.  ").lower()
 
     if equation == "q":
-        print("Thanks for testing my app!")
+        print("\nThanks for testing my app!")
         break
 
-    while equation == "1" or equation =="2":
-        # time = input("\nAnd what value would you like to use for delta t (change in time) in these equations?  ")
-        # time = float(time)
-
-        if equation == "1":
-            i = 1
-            while i < 3:
-                radius = input("\nPlease enter the initial radius you would like to use.  ")
-                d_radius = input("\nPlease enter the amount you would like the radius to change each time.  ")
-                sphere_list = list()
-                sphere_dict = {}
+    if equation == "1":
+        i = 1
+        while i < 4:
+            radius = input("\nPlease enter the initial radius you would like to use.  ")
+            d_radius = input("\nPlease enter the amount you would like the radius to change each time.  ")
+            # sphere_list = list()
+            sphere_dict = {}
+            try:
                 sphere(radius, d_radius)
-                i += 1
-            else:
-                print("\nPlease check your results in the file '5d_results.py' in your folder.\nNew results will be appended to the end of the file each time you run this.\n")
-                break
-            
-        elif equation == "2":
-            i = 1
-            while i < 2:
-                print ("\nFor a rectangular prism, please choose the intial value of each side (x, y, z) and the change of each of these values.")
-                x_length = input("\nWhat is the initial length (x) for the prism?  ")
-                dx = input("\nWhat is the change in x (delta x) after each iteration?  ")
-                y_width = input("\nWhat is the initial width (y) for the prism?  ")
-                dy = input("\nWhat is the change in y (delta y) after each iteration?  ")
-                z_height = input("\nWhat is the initial height (z) for the prism?  ")
-                dz = input("\nWhat is the change in z (delta z) after each iteration?  ")
-                prism_list = list()
-                prism_dict = {}
+            except ValueError:
+                print("\nPlease enter numeric (integer or float) values only.")
+                continue
+            prt_log(sphere_dict)
+            print("\n")
+            print(sphere_dict)
+            print("\nYou will also see these results in the file '5d_results.py' in your folder where they have been saved for future use.\nNew results will be appended to the end of the file each time you run this loop.\n")
+            i += 1
+        
+    elif equation == "2":
+        i = 1
+        while i < 2:
+            print ("\nFor a rectangular prism, please choose the intial value of each side (x, y, z) and the change of each of these values.")
+            x_length = input("\nWhat is the initial length (x) for the prism?  ")
+            dx = input("\nWhat is the change in x (delta x) after each iteration?  ")
+            y_width = input("\nWhat is the initial width (y) for the prism?  ")
+            dy = input("\nWhat is the change in y (delta y) after each iteration?  ")
+            z_height = input("\nWhat is the initial height (z) for the prism?  ")
+            dz = input("\nWhat is the change in z (delta z) after each iteration?  ")
+            prism_dict = {}
+            try:
                 r_prism(x_length, dx, y_width, dy, z_height, dz)
-                i += 1
-            else:
-                break
+            except ValueError:
+                print("\nPlease enter numeric (integer or float) values only.")
+            prt_log(prism_dict)
+            print("\n")
+            print(prism_dict)
+            print("\nYou will also see these results in the file '5d_results.py' in your folder where they have been saved for future use.\nNew results will be appended to the end of the file each time you run this loop.\n")
+            i += 1
--- README.txt
+++ README.txt
@@ -1,14 +1,13 @@
-This is my Python class project. It is a basic 5d Modeler.
+This is my Python class project. It is a basic 5d Modeler. This is also the precursor for a version that will output the resulting values in a graphic form, but this was not a requirement for this class and it would have taken more time than was available to develop a graphical output. However, I have included the necessary output to a json file which can later be used to develop a graphical output. This output will be saved in a file (called 5d_modeler.txt) in the same folder from which the user runs this app.
 
-The idea is to offer the user a choice to model either a sphere or rectangulal prism then the user will choose the starting values, the change in time, and the change from the starting value over five iterations.
+The idea is to offer the user a choice to model either a sphere or rectangular prism (or to quit). Then the user will choose the starting values for the chosen shape and the amount of change from the starting value over three iterations. The values will be printed in the console and save in the text file mentioned previously.
 
-The app will process the output of the starting value plus four more iterations of the starting value plus the change (delta) to create five separate sets of space-time equations.
+Each time the user chooses to run a particular shape, they are modeling one specific reality where the chosen object increases in size based on the user's initial input. The data created each time the user runs through a loop and chooses a new shape or new values creates the multiple sets of objects changing over time (a fifth dimensional set).
 
 The requirements that will be met are:
 	1. Implement a “master loop” console application where the user can repeatedly enter commands/perform actions, including choosing to exit the program
-	2. Create a dictionary or list, populate it with several values, retrieve at least one value, and use it in your program
-	3. Implement a log that records errors, invalid inputs, or other important events and writes them to a text file
-	4. Read data from an external file, such as text, JSON, CSV, etc and use that data in your application
-	5. Create and call at least 3 functions or methods, at least one of which must return a value that is used somewhere else in your code. To clarify, at least one function should be called in your code, that function should calculate, retrieve, or otherwise set the value of a variable or data structure, return a value to where it was called, and use that value somewhere else in your code. For example, you could create a function that reads how many items there are in a text file, returns that value, and later uses that value to execute a loop a certain number of times.
+	2. Implement a log that records errors, invalid inputs, or other important events and writes them to a text file
+	3. Create and call at least 3 functions or methods, at least one of which must return a value that is used somewhere else in your code. To clarify, at least one function should be called in your code, that function should calculate, retrieve, or otherwise set the value of a variable or data structure, return a value to where it was called, and use that value somewhere else in your code.
+	4. Build a conversion tool that converts user input to another type and displays it.
 
-The user only needs to have Python installed to run this (it is a python console app). The log that is created with all the data should appear in whatever folder the user runs the app from.
+The user only needs to have Python installed to run this (it is a python console app). The log that is created with all the data will be generated and updated in whatever folder the user runs the app from.
